db7cca0e5d7791ead327a7673be6e0a3b750bcb3
Pat <patrick.h.needham@gmail.com>
2018-08-15 16:16:50 -0400

    got axios to work with cors -- just use path; npm install removed some node modules

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ListsContainer extends Component {
-    render() {
-        return (
-            <div className="Lists-container">
-                Lists
-            </div>
-        )
+  constructor(props){
+    super(props)
+    this.state = {
+      lists: []
     }
-}
+  }
 
+  componentDidMount() {
+    // https://stackoverflow.com/questions/44932650/running-a-react-rails-app-locally
+    axios.get('api/v1/lists.json')
+    .then(response => {
+      console.log(response)
+      this.setState({
+        lists: response.data
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="Lists-container">
+        Lists
+      </div>
+    )
+  }
+}
 export default ListsContainer;
